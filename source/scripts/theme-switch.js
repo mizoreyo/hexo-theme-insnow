e374a762d045cd18489141162aa5c170e162f7a9
mizoreyo <mizoreyo@outlook.com>
2022-06-15 21:44:27 +0800

    add dark mode and fixed some bugs

--- source/scripts/theme-switch.js
+++
@@ -1,6 +0,0 @@
-function switchTheme() {
-  let cssElement = document.createElement('link');
-  cssElement.setAttribute('rel', 'stylesheet');
-  cssElement.setAttribute('href', '/css/prism/material-dark.css');
-  document.body.appendChild(cssElement);
-}
